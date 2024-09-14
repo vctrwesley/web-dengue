69c3221b8a21f22661a34c509dd7ddbc40a9b869
Felipe Nogueira <felipe.leite23@gmail.com>
2024-09-14 01:53:35 -0300

    footer responsivo

--- src/app/template/footer/footer.component.ts
+++ src/app/template/footer/footer.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    this.onResize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: Event): void {
+    const width = window.innerWidth;
+    const linksContainer = this.el.nativeElement.querySelector('.links-container');
+    const social = this.el.nativeElement.querySelector('#social');
+    const footerLogo = this.el.nativeElement.querySelector('.footer-logo');
+    const footerMenu = this.el.nativeElement.querySelector('.footer-menu');
+
+    if (width <= 768) {
+      if (linksContainer && social && footerLogo) {
+        linksContainer.insertBefore(social, linksContainer.firstChild);
+        linksContainer.insertBefore(footerLogo, linksContainer.firstChild);
+      }
+    } else {
+      if (linksContainer && social && footerLogo) {
+        linksContainer.appendChild(social);
+        footerMenu.insertBefore(footerLogo, footerMenu.firstChild);
+      }
+    }
+  }
 }
