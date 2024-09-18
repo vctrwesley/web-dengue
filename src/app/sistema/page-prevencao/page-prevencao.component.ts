fd11203b54bec04ae0efcf2aa0de1f23a22b0d37
JVictor011 <jvictor2021@alu.ufc.br>
2024-09-17 21:33:51 -0300

    merge

--- src/app/sistema/page-prevencao/page-prevencao.component.ts
+++ src/app/sistema/page-prevencao/page-prevencao.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-page-prevencao',
   templateUrl: './page-prevencao.component.html',
-  styleUrl: './page-prevencao.component.css'
+  styleUrls: ['./page-prevencao.component.css'],
 })
 export class PagePrevencaoComponent {
+  currentMargin = 0; // margem inicial
 
+  // Defina URLs diretamente no HTML, não é necessário incluir aqui
+
+  slideRight() {
+    const slideWidth = 25; // porcentagem do slide
+    const maxMargin = -(100 - slideWidth); // margem máxima negativa
+
+    if (this.currentMargin > maxMargin) {
+      this.currentMargin -= slideWidth;
+    }
+  }
+
+  slideLeft() {
+    const slideWidth = 25; // porcentagem do slide
+
+    if (this.currentMargin < 0) {
+      this.currentMargin += slideWidth;
+    }
+  }
 }
