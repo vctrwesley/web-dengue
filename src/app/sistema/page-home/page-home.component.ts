c4c8713024c68f129f8c267d1a7e57c0a1bb1d44
vctrwesley <vctrwesley@gmail.com>
2024-09-22 12:00:05 -0300

    subindo base e testes de carrosel junto de aumento da memoria da aplicacao

--- src/app/sistema/page-home/page-home.component.ts
+++ src/app/sistema/page-home/page-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-page-home',
@@ -9,6 +9,17 @@ export class PageHomeComponent {
   text: string = 'Combate ao mosquito';
   repeatedText: string[] = [];
 
+  @ViewChild('carousel') carousel!: ElementRef;
+
+  items = [
+    { title: 'Card 1', image: './assets/images/page-home/card-feedback.png' },
+    { title: 'Card 2', image: './assets/images/page-home/card-feedback.png' },
+    { title: 'Card 3', image: './assets/images/page-home/card-feedback.png' },
+    { title: 'Card 4', image: './assets/images/page-home/card-feedback.png' },
+    { title: 'Card 5', image: './assets/images/page-home/card-feedback.png' },
+    { title: 'Card 6', image: './assets/images/page-home/card-feedback.png' },
+  ];
+
   constructor() {
     this.repeatText(15); // Repetir o suficiente para preencher a tela
   }
@@ -18,4 +29,27 @@ export class PageHomeComponent {
       this.repeatedText.push(this.text);
     }
   }
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    this.startAutoplay();
+  }
+  
+  // Funções de rolagem
+  scrollLeft(carousel: HTMLElement) {
+    carousel.scrollBy({ left: -carousel.clientWidth, behavior: 'smooth' });
+  }
+
+  scrollRight(carousel: HTMLElement) {
+    carousel.scrollBy({ left: carousel.clientWidth, behavior: 'smooth' });
+  }
+
+  startAutoplay() {
+    const carouselElement = this.carousel.nativeElement;
+    setInterval(() => {
+      this.scrollRight(carouselElement);
+    }, 3000); // Ajuste o intervalo conforme necessário
+  }
+  
 }
