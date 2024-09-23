5d4572978e1e1fb0e02661dc05987845eca7faeb
vctrwesley <vctrwesley@gmail.com>
2024-09-23 01:04:08 -0300

    duas novas paginas de politica e termos e ajuste de pointer na navbar

---
+++ src/app/sistema/page-politica-de-privacidade/page-politica-de-privacidade.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-politica-de-privacidade',
+  templateUrl: './page-politica-de-privacidade.component.html',
+  styleUrl: './page-politica-de-privacidade.component.css'
+})
+export class PagePoliticaDePrivacidadeComponent {
+
+}
